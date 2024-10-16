be7c0cbb95bdec0b51b23876d5ef250251c4c7d6
adrHuerta <adrhuerta@gmail.com>
2024-10-16 18:19:32 +0200

    adding new criteria same data func

--- R/01_unification/unification-and-matching.R
+++ R/01_unification/unification-and-matching.R
@@ -92,6 +92,7 @@ deciding_matching_25km <- function(list_xy,
 
   val_list_xy <- val_database[, list_xy]
 
+  # check based on r and mae
   r_nearby <-
     lapply(c(-1, 0, 1),
            function(idd) {
@@ -155,8 +156,68 @@ deciding_matching_25km <- function(list_xy,
   rmae_id <- rmae_df[rmae_df$rval > 0.9999 & rmae_df$maeval < 0.1, ]$ID
   rmae_id <- unique(rmae_id[!is.na(rmae_id)])
 
-  # size
-  lengh_values_xy <- colSums(!is.na(val_list_xy[, rmae_id, drop = FALSE]))
+  # check based on same values
+  # same values above 0.5 mm
+  val_list_xy_comp <- val_list_xy
+  val_list_xy_comp[val_list_xy_comp < 0.5] <- 0
+
+  check_same_values <- lapply(
+    seq_along(list_xy)[2:length(list_xy)],
+    function(i) {
+
+      n_same_values_df <- data.frame(target = val_list_xy_comp[, 1,],
+                                     nearby = val_list_xy_comp[, i])
+      n_same_values_df <- n_same_values_df[complete.cases(n_same_values_df), ]
+
+      n_same_values_df <- transform(n_same_values_df,
+                                    dif = target - nearby,
+                                    is0 = (target + nearby) == 0)
+
+      n_same_values_df <- n_same_values_df[
+        n_same_values_df$is0 == FALSE,
+      ]
+
+      n_same_values_df <- n_same_values_df[
+        n_same_values_df$dif == 0,
+      ]
+
+      all_values_target <- val_list_xy_comp[, 1]
+      all_values_target <- sum(!is.na(all_values_target))
+
+      all_values_nearby <- val_list_xy_comp[, i]
+      all_values_nearby <- sum(!is.na(all_values_nearby))
+
+      n_same_values <- length(n_same_values_df$target)
+      target_percent <- n_same_values  * 100 / all_values_target
+      nearby_percent <- n_same_values  * 100 / all_values_nearby
+
+      # 50% of the values are the same in both stations
+      if (target_percent > 50 & nearby_percent > 50) {
+
+        out1 <- colnames(val_list_xy_comp[, 1, drop = FALSE])
+        out2 <- colnames(val_list_xy_comp[, i, drop = FALSE])
+        out <- c(out1, out2)
+
+      } else {
+
+        out <- NULL
+
+      }
+
+      return(out)
+
+    }
+  )
+  check_same_values <- unlist(check_same_values)
+
+  # combine all coinditions
+  all_id <- c(rmae_id, check_same_values)
+  all_id <- unique(all_id)
+
+  # deciding by size
+  # if two stations have the same size, both are removed
+  # in the final procedure
+  lengh_values_xy <- colSums(!is.na(val_list_xy[, all_id, drop = FALSE]))
 
   choosen_station <- names(which.max(lengh_values_xy))
   choosen_station <- as.character(choosen_station)
