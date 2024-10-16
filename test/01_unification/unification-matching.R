9037801876c90cfc381e24e06024c35a99f34414
adrHuerta <adrhuerta@gmail.com>
2024-10-16 18:19:10 +0200

    new criteria same data

--- test/01_unification/unification-matching.R
+++ test/01_unification/unification-matching.R
@@ -47,16 +47,18 @@ get_nearby_points(xy_target = "id_AR_122",
 plot(raw_data$data[, c("id_AR_122")],
      raw_data$data[, c("id_AR_040")])
 
-#
-get_nearby_points(xy_target = "id_BO3115",
+# special case when not all the data is the same (r > 0.999 & mae < 0.1)
+# the original algorithm could not find the match
+# but the new algorithm can
+get_nearby_points(xy_target = "id_SU_300",
                   xy_database = raw_data$xyz,
                   lmt_xy = 25 * 1000,
                   lmt_n = NA) %>%
   deciding_matching_25km(list_xy = .,
                          val_database = raw_data$data)
 
-plot(raw_data$data[, c("id_BO3332")],
-     raw_data$data[, c("id_BO3115")])
+plot(raw_data$data[, c("id_SU_300")],
+     raw_data$data[, c("id_SU_317")])
 
 #
 xyz_vect <- terra::vect(
